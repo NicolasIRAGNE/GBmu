c60c9467f12d48faae2c36962141d78f1ed06ee2
niragne <niragne@student.42.fr>
2021-12-11 21:34:42 +0100

    ADD TESTS

---
+++ tests/test_op_add.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file test_op_add.c
+ * @author Nicolas IRAGNE (nicolas.iragne@alyce.fr)
+ * @brief
+ * @date 2021-12-11
+ */
+
+extern "C"
+{
+#include "gb.h"
+#include "op.h"
+}
+#include "gtest/gtest.h"
+
