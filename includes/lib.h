65988b9c4f903cf3297664f7d70c2e5917cfdbed
Cedric M'PASSI <cempassi@student.42.fr>
2022-01-05 00:58:06 +0100

    Add lib.h and lib.c

---
+++ includes/lib.h
@@ -0,0 +1,10 @@
+#ifndef LIB_H
+# define LIB_H
+
+#include "gbmu.h"
+
+extern struct gb_cpu_s	gb_global;
+
+void test_link();
+
+#endif
---
+++ srcs/lib.c
@@ -0,0 +1,6 @@
+#include "lib.h"
+
+void test_link()
+{
+    printf("Called!");
+}
