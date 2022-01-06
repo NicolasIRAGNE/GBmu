7cae9610030004405d1dd61852708eaf4656d97b
Cedric M'PASSI <cempassi@student.42.fr>
2022-01-06 08:23:39 +0100

    Cleanup main

--- crates/gbmu/src/main.rs
+++ crates/gbmu/src/main.rs
@@ -1,17 +1,15 @@
 mod system;
 mod emulator;
 
-use std::fs;
 use std::path::PathBuf;
 use std::process;
 
 use log::*;
-use anyhow::{Context, Ok, Result};
+use anyhow::{Ok, Result};
 use structopt::StructOpt;
 use structopt::clap::AppSettings::*;
 
 use system::System;
-use bindings::gb::*;
 
 #[derive(Debug, StructOpt)]
 #[structopt(setting(ColorAuto), setting(ColoredHelp))]
@@ -21,15 +19,6 @@ struct Opt {
     #[structopt(long, short, parse(from_os_str))]
     rom: PathBuf,
 
-    /// A file containing a binary dump of the Game Boy BIOS.
-    ///
-    /// If not supplied, the emulator will begin executing the ROM as if the BIOS had succeeded.
-    #[structopt(long)]
-    bios: Option<PathBuf>,
-
-    /// Enable debug mode.
-    #[structopt(short, long)]
-    debug: bool,
 }
 
 fn run(opt: Opt) -> Result<()> {
@@ -37,11 +26,6 @@ fn run(opt: Opt) -> Result<()> {
 
     //let mut emulator = builder.build();
 
-    if let Some(bios) = &opt.bios {
-        info!("loading BIOS from file '{}'", bios.display());
-        let bios = fs::read(&bios).context("could not read BIOS")?;
-        //emulator.load_bios(&bios).context("could not load BIOS")?;
-    }
 
     info!("loading ROM from file '{}'", opt.rom.display());
     let rom = opt.rom.into_os_string().into_string().unwrap();
